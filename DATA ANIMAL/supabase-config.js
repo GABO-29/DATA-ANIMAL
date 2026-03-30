4f00a06afa5049fa5aa5b28f3f1afb8f46a2b040
GABO-29 <denyson7blanco29@gmail.com>
2026-03-30 17:42:06 -0400

    Add files via upload

--- DATA ANIMAL/supabase-config.js
+++ DATA ANIMAL/supabase-config.js
@@ -1,6 +1,3 @@
-// supabase-config.js
 const SUPABASE_URL = 'https://vbbjcdwoksyhjararssg.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_aslH_NoPhM4zT7YjUv2elw_wV65l06u';
-
-// Inicializamos el cliente de Supabase de forma global
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
